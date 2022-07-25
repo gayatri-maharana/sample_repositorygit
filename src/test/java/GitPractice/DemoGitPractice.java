0c0625aac0ddb2ba881da112abed1cdd31b2a823
gayatri-maharana <gayatrimaharana786@gmail.com>
2022-07-25 13:21:20 +0530

    first commit

---
+++ src/test/java/GitPractice/DemoGitPractice.java
@@ -0,0 +1,13 @@
+package GitPractice;
+
+import org.testng.annotations.Test;
+
+public class DemoGitPractice {
+@Test
+public void practiceGit()
+{
+	System.out.println("hi");
+	System.out.println("hello");
+}
+
+}
